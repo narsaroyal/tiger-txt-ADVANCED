66b2acf3bf33137f4f670d5f749d313bd8e4d053
narsaroyal <167844054+narsaroyal@users.noreply.github.com>
2024-05-01 22:11:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ LOG = -1002089257258
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5987970971").split()):
+    for x in (os.environ.get("ADMINS", "1739695986").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
